7dfadf9c936f713f998c4a939795837ff741e87c
mustafa dokumacı <mustafa@foraa.com>
2014-09-22 16:06:46 +0300

    bug fix

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -4,19 +4,15 @@ var fs = require('fs');
 var path = require('path');
 
 var localFile = path.normalize(__dirname + '/../config/channel.js');
-var projectPath = path.normalize(process.cwd() + '/../../config/plugins');
 var projectFile = path.normalize(process.cwd() + '/../../config/plugins/ah-broadcast-plugin.js');
 
 if(!fs.existsSync(process.cwd() + '/../../config/plugins')) {
-	fs.mkdirSync(process.cwd() + '/../../config/plugins');
+	try {
+		fs.mkdirSync(process.cwd() + '/../../config/plugins');
+	} catch (e) { }
 }
 
 if(!fs.existsSync(projectFile)){
 	console.log("copying " + localFile + " to " + projectFile);
-
-	try {
-		fs.mkdirSync(projectPath);
-	} catch (e) { }
-
 	fs.createReadStream(localFile).pipe(fs.createWriteStream(projectFile));
 }
