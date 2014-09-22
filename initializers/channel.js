588f6244ebe14400efb9716fd215e972f390814b
mustafa dokumacı <mustafa@foraa.com>
2014-09-22 22:40:27 +0300

    remove being destroyed connection from connection set of every channel

--- initializers/channel.js
+++ initializers/channel.js
@@ -3,22 +3,43 @@ var async = require('async');
 
 exports.channel = function (api, next) {
 	var redis = api.redis.client;
-	var prefix = 'channels:';
+
+	var channel_prefix = 'connections_in_channel:';
+	var connection_prefix = 'channels_in_connection:';
 
 	api.channel = {};
 
+	// Add connection id to connection set of channel
+	// Add channel id to channel set of connection
 	api.channel.join = function (connection_id, channel_id, next) {
-		redis.sadd(prefix + channel_id, connection_id, next);
+		async.parallel([
+			function (cb) {
+				redis.sadd(channel_prefix + channel_id, connection_id, cb);
+			},
+			function (cb) {
+				redis.sadd(connection_prefix + connection_id, channel_id, cb);
+			}
+		], next);
 	};
 
+	// Remove connection id from connection set of channel
+	// Remove channel id from channel set of connection
 	api.channel.leave = function (connection_id, channel_id, next) {
-		redis.srem(prefix + channel_id, connection_id, next);
+		async.parallel([
+			function (cb) {
+				redis.srem(channel_prefix + channel_id, connection_id, cb);
+			},
+			function (cb) {
+				redis.srem(connection_prefix + connection_id, channel_id, cb);
+			}
+		], next);
 	};
 
+	// Send messsage to every connection in connection set of channel
 	api.channel.broadcast = function (channel_id, data, next) {
 		async.waterfall([
 			function (cb) {
-				redis.smembers(prefix + channel_id, cb);
+				redis.smembers(channel_prefix + channel_id, cb);
 			},
 			function (list, cb) {
 				async.each(
@@ -32,8 +53,31 @@ exports.channel = function (api, next) {
 		], next);
 	};
 
+	// Join every connection to a channel named 'all'
 	api.connections.addCreateCallback(function (connection) {
 		api.channel.join(connection.id, 'all');
+	});
+
+	// Remove being destroyed connection from connection set of every channel
+	// Also drop channel set of being destroyed connection
+	api.connections.addDestroyCallback(function (connection) {
+		async.waterfall([
+			function (cb) {
+				redis.smembers(connection_prefix + connection.id, cb);
+			},
+			function (list, cb) {
+				async.each(
+					list,
+					function (channel_id, cb) {
+						redis.srem(channel_prefix + channel_id, connection.id, cb);
+					},
+					cb
+				);
+			},
+			function (cb) {
+				redis.del(connection_prefix + connection.id, cb);
+			}
+		], next);
 	});
 
 	next();
